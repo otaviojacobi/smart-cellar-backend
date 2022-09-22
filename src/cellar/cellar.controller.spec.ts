e0e591e0e0abf08307bd174aa26417cfb61a68e4
Otávio Jacobi <otaviojacobi@gmail.com>
2022-09-22 14:39:42 -0300

    specs

--- src/cellar/cellar.controller.spec.ts
+++ src/cellar/cellar.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CellarController } from './cellar.controller';
 import { CellarService } from './cellar.service';
+import { Cellar } from './entities/cellar.entity';
 
 describe('CellarController', () => {
   let controller: CellarController;
@@ -8,7 +10,13 @@ describe('CellarController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CellarController],
-      providers: [CellarService],
+      providers: [
+        CellarService,
+        {
+          provide: getRepositoryToken(Cellar),
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     controller = module.get<CellarController>(CellarController);
--- src/cellar/cellar.service.spec.ts
+++ src/cellar/cellar.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CellarService } from './cellar.service';
+import { Cellar } from './entities/cellar.entity';
 
 describe('CellarService', () => {
   let service: CellarService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CellarService],
+      providers: [
+        CellarService,
+        {
+          provide: getRepositoryToken(Cellar),
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     service = module.get<CellarService>(CellarService);
